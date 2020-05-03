959850eb31a45c01350c9030c73ea0ba6fc95ad6
Vinay <vchadala@berkeley.edu>
2020-05-02 19:22:54 -0700

    Improvements to Dom Set

--- solver.py
+++ solver.py
@@ -21,9 +21,7 @@ def solve(G):
     if not nx.number_of_edges(G):
         return G, 'None'
 
-    
     # nx documentation here: https://networkx.github.io/documentation/stable/tutorial.html
-
 
         # Method 1: MST Pruning
 
@@ -34,7 +32,6 @@ def solve(G):
     while iters > 0:
         min_spanning_tree, iters = remove_leaves(min_spanning_tree, G)
 
-
         # Method 1.5: Another version of MST: remove based on cost
 
     min_spanning_tree_cost = nx.minimum_spanning_tree(G, 'weight')
@@ -42,15 +39,16 @@ def solve(G):
     while iters > 0:
         min_spanning_tree_cost, iters = remove_leaves_cost(min_spanning_tree_cost)
 
-
-
         # Method 2: Dominating Set
 
     # set the weight of each node (to be used in the
     # calculation of dominating set)
+    centerList = nx.center(G)
+    # calculate distances of all vertices to center
+    distanceToCenters, pathToCenters = nx.algorithms.shortest_paths.weighted.multi_source_dijkstra(G, centerList, weight='weight')
+    # set the weights for dom set
     for v in G.nodes:
-        G.nodes[v]['w'] = -1 * G.degree[v]
-        
+        G.nodes[v]['w'] = distanceToCenters[v]
     # calculate a dominating set, using a provided algo
     dom_set = list(nx.algorithms.approximation.dominating_set.min_weighted_dominating_set(G, weight='w'))
 
@@ -65,34 +63,37 @@ def solve(G):
     for i in range(len(dom_set)):
         v = dom_set[i]
         #print("i: " + str(i) + ", dom_set[i]: " + str(v))
-        for j in range(i + 1, len(dom_set)):
-            u = dom_set[j]
-            #print("j: " + str(j) + ", dom_set[j]: " + str(u))
-            path = shortest_paths[v][u]
-            #print(path)
-            lis = []
-            for i in range(len(path) - 1):
-                v = path[i]
-                u = path[i+1]
-                p = 0
-                try:
-                    # this errors if the edge does not exist
-                    p = X.edges[v, u]
-                except:
-                    # if the edge deosn't exist, add it
-                    w = G.edges[v, u]['weight']
-                    e = (v, u, w)
-                    #print(e)
-                    lis.append(e)
-            #print(paths)
-            if (lis):
-                X.add_weighted_edges_from(lis)
+        for j in range(len(dom_set)):
+            if(i != j):
+                u = dom_set[j]
+                #print("j: " + str(j) + ", dom_set[j]: " + str(u))
+                path = shortest_paths[v][u]
+                # print(path)
+                lis = []
+                for i in range(len(path) - 1):
+                    v = path[i]
+                    u = path[i + 1]
+                    p = 0
+                    try:
+                        # this errors if the edge does not exist
+                        p = X.edges[v, u]
+                    except:
+                        # if the edge deosn't exist, add it
+                        w = G.edges[v, u]['weight']
+                        e = (v, u, w)
+                        # print(e)
+                        lis.append(e)
+                # print(paths)
+                if (lis):
+                    X.add_weighted_edges_from(lis)
     dom_mst = nx.minimum_spanning_tree(X, 'weight')
+    iters = 1
+    while iters > 0:
+        dom_mst, iters = remove_leaves_cost(dom_mst)
 
+    # Final section: display data and choose best method
 
-        # Final section: display data and choose best method
-
-    dom = ['none', float('inf'), None ]
+    dom = ['none', float('inf'), None]
     # sometimes the dominating set method produces a null value.
     # this typically happens on graphs where the best answer
     # is a single vertex, with no edges. We should fix this bug.
@@ -110,13 +111,10 @@ def solve(G):
     cost3 = average_pairwise_distance_fast(min_spanning_tree_cost)
     cost_mst = ["Cost-First MST", cost3, min_spanning_tree_cost]
     print("Cost-First MST: " + str(cost3))
- 
+
     best = min([dom, mst, cost_mst], key=lambda x: x[1])
     print(best[0] + ' was best!')
     return best[2], best[0]
-
-
-
 
 
 # Handles removing appropriate leaves from
@@ -144,11 +142,9 @@ def remove_leaves(min_tree, G):
     return min_tree, iters
 
 
-
-
 # This method removes leaves from a given min_tree
 # if the tree remains a dominating set on G
-# [previous behavior: remove if doing so decreases 
+# [previous behavior: remove if doing so decreases
 # the cost (average pairwise distance)]
 def prune_leaves(leaves, min_tree, G):
     removed = 0
@@ -162,12 +158,12 @@ def prune_leaves(leaves, min_tree, G):
             #cost = average_pairwise_distance_fast(min_tree)
             min_tree.remove_node(leaf)
             #new_cost = average_pairwise_distance_fast(min_tree)
-            #if (new_cost > cost):
+            # if (new_cost > cost):
 
-            ############## IMPLEMENT DP HERE? 
-            ############## choose between keeping node and removing it
-            #### like choose min_cost(keeping_node, removing_node)
-            ### idk how to implement tho
+            # IMPLEMENT DP HERE?
+            # choose between keeping node and removing it
+            # like choose min_cost(keeping_node, removing_node)
+            # idk how to implement tho
             if (not nx.is_dominating_set(G, min_tree.nodes)):
                 min_tree.add_node(leaf)
                 min_tree.add_edge(e[0], e[1], weight=e[2])
@@ -178,9 +174,6 @@ def prune_leaves(leaves, min_tree, G):
     if (removed):
         print("(Removal) We removed " + str(removed) + " leaves in this iter")
     return min_tree, kept_leaves, removed
-
-
-
 
 
 # Handles removing appropriate leaves from
@@ -203,8 +196,7 @@ def remove_leaves_cost(min_tree):
     return min_tree, iters
 
 
-
-# [previous behavior: remove if doing so decreases 
+# [previous behavior: remove if doing so decreases
 # the cost (average pairwise distance)]
 def prune_leaves_cost(leaves, min_tree):
     removed = 0
@@ -239,10 +231,8 @@ def print_leaves(leaves, min_tree):
     print(lis)
 
 
-
-
-
 # To run: python3 solver.py inputs
+
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2
@@ -255,7 +245,7 @@ if __name__ == '__main__':
         G = read_input_file(p + '/' + path)
         T, met = solve(G)
         if (met in methods):
-            methods[met] += 1 
+            methods[met] += 1
         else:
             methods[met] = 1
         assert is_valid_network(G, T)
@@ -267,26 +257,15 @@ if __name__ == '__main__':
         read_output_file(out, G)
         total += 1
     best_method = max(methods, key=methods.get)
-    percent = (float) (methods[best_method] * 100) / total
+    percent = (float)(methods[best_method] * 100) / total
     print("The best method is " + str(best_method) + ", at " + str(percent) + "%")
-
-
-
-
-
-
-
-
-
-
-
 
     # Avg-weight/Cost-Checking Hybird Method:
     # this is outperformed by full cost-checking method,
     # an I'm not sure why. rip. this is what it does:
     # First, Use avg to cull obvious large leaf edges:
-    # Remove edges with weights that are 
-    # greater than or equal to the average, 
+    # Remove edges with weights that are
+    # greater than or equal to the average,
     # and recalculate the average.
     # Then use cost-checking for the rest
 
